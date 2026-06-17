e1e5472aa8245e5ae7b867289a78169002504531
Shivira-Stack <shivirastack@gmail.com>
2026-06-17 23:37:30 +0530

    All Web Copy Items Correction

--- apps/web/src/navigation_routes.jsx
+++ apps/web/src/navigation_routes.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import { lazy, Suspense } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+// const Home = lazy(() => import("../pages/Home"));
+const AuthLayout = lazy(() => import("./layouts/auth_layout"));
+const MainLayout = lazy(() => import("./layouts/main_layout"));
+const ErrorLayout = lazy(() => import("./layouts/error_layout"));
 
 function Main() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>Hello World!</div>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                <Route path='/auth' element={<AuthLayout />}>
+                    <Route path="login" index element={<h1>login</h1>} />
+                </Route>
+                <Route path='/' element={<MainLayout />}>
+                    <Route path="dashboard" element={<h1>dashboard</h1>} />
+                </Route>
+
+                <Route path="*" element={<ErrorLayout><h1>Page Not Found</h1></ErrorLayout>} />
+
+            </Routes>
+        </Suspense>
+        </BrowserRouter>
     </>
   )
 }
