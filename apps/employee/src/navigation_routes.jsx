fbf7a52b6c1eb695242027cc7441181eb2bf2b65
Shivira-Stack <shivirastack@gmail.com>
2026-06-18 00:18:51 +0530

    All Web SEO Component Create

--- apps/employee/src/navigation_routes.jsx
+++ apps/employee/src/navigation_routes.jsx
@@ -1,32 +1,37 @@
 import { lazy, Suspense } from "react";
+import { HelmetProvider } from "react-helmet-async";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 // const Home = lazy(() => import("../pages/Home"));
 const AuthLayout = lazy(() => import("./layouts/auth_layout"));
+const SignIn = lazy(() => import("./pages/auth/sign_in"));
+
 const MainLayout = lazy(() => import("./layouts/main_layout"));
 const ErrorLayout = lazy(() => import("./layouts/error_layout"));
 
 function Main() {
 
-  return (
-    <>
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                <Route path='/auth' element={<AuthLayout />}>
-                    <Route path="login" index element={<h1>login</h1>} />
-                </Route>
-                <Route path='/' element={<MainLayout />}>
-                    <Route path="dashboard" element={<h1>dashboard</h1>} />
-                </Route>
+    return (
+        <>
+            <HelmetProvider>
+                <BrowserRouter>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Routes>
+                            <Route path='/auth' element={<AuthLayout />}>
+                                <Route path="sign_in" index element={<SignIn />} />
+                            </Route>
+                            <Route path='/' element={<MainLayout />}>
+                                <Route path="dashboard" element={<h1>dashboard</h1>} />
+                            </Route>
 
-                <Route path="*" element={<ErrorLayout><h1>Page Not Found</h1></ErrorLayout>} />
+                            <Route path="*" element={<ErrorLayout><h1>Page Not Found</h1></ErrorLayout>} />
 
-            </Routes>
-        </Suspense>
-        </BrowserRouter>
-    </>
-  )
+                        </Routes>
+                    </Suspense>
+                </BrowserRouter>
+            </HelmetProvider>
+        </>
+    )
 }
 
 export default Main;
---
+++ apps/employee/src/pages/auth/sign_in.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Seo from '../../contexts/seo_contexts';
+
+function SignIn() {
+    return (
+        <>
+            <Seo
+                title="Sign In | Shivira Stack"
+                description="Welcome to Shivira Stack"
+                keywords="shivira, stack, software"
+            />
+
+            <h1>Home Page</h1>
+        </>
+    )
+}
+
+export default SignIn;
