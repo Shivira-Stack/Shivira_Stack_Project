14039b130f01f212de775b93e49785fea70d20dc
Shivira-Stack <shivirastack@gmail.com>
2026-06-20 14:36:35 +0530

    Head Upgrade Adding Style on Head For No Over Write Any Css Main Values

--- apps/admin/src/contexts/vendors_contexts.jsx
+++ apps/admin/src/contexts/vendors_contexts.jsx
@@ -4,6 +4,8 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 function VendorsHead() {
     useEffect(() => {
+
+        // List of vendor file
         const assets = [
             {
                 type: "css",
@@ -25,8 +27,13 @@ function VendorsHead() {
                 type: "js",
                 src: `${API_URL}/vendors/bootstrap/js/bootstrap.min.js`,
             },
+            {
+                type: "css",
+                href: `${API_URL}/fonts/mont/montfont.css`,
+            },
         ];
 
+        // Dynamic vendor list place
         assets.forEach((asset) => {
             if (asset.type === "css") {
                 const link = document.createElement("link");
@@ -41,6 +48,27 @@ function VendorsHead() {
                 document.head.appendChild(script);
             }
         });
+
+        // Custom styles
+        const style = document.createElement("style");
+        style.textContent = `
+            :root {
+                --Stack-Font: 'Mont', sans-serif;
+            }
+            
+            *{
+                margin: 0;
+                padding: 0;
+            }
+            
+            body {
+                font-family: var(--Stack-Font);
+            }
+
+        `;
+
+        document.head.appendChild(style);
+
     }, []);
 
     return null;
